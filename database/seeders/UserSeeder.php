ed1f4f5df802825ea86d713905b6e140ca76206a
Zakir Hossen <web.zakirbd@gmail.com>
2021-11-12 15:37:36 +0600

    dev: User seeder created

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $data = [
+            'name' => 'Zakir Hossen',
+            'email' => 'web.zakirbd@gmail.com',
+            'password' => bcrypt('password')
+        ];
+
+        User::create($data);
+    }
+}
