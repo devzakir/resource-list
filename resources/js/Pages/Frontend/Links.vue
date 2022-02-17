4cec61d30040b6bdba502f691d818b66c35ed3e5
Ariful Islam Arif <arif14arif15@gmail.com>
2022-02-17 18:55:23 +0600

    links list improved

--- resources/js/Pages/Frontend/Links.vue
+++ resources/js/Pages/Frontend/Links.vue
@@ -1,11 +1,15 @@
 <template>
     <Head title="Links" />
     <Navbar />
-        <div class="flex justify-center">
-            <div class="flex flex-wrap ">
+        <div class="flex">
+            <div class="flex flex-wrap justify-center">
                 <div class="w-full p-3 " v-for="link in links" :key="link.id">
                     <div class="p-4 rounded-xl shadow-md bg-gray-600 text-white">
-                        <h4 class="mb-2 text-lg font-semibold">{{ link.title }}</h4>
+                        <h4 class="mb-2 text-lg font-semibold">
+                            <a href="http://" style="text-decoration:underline">{{ link.title }}</a>
+                        </h4>
+                         <p class="text-base" v-html="link.description"/>
+
                     </div>
                 </div>
             </div>
